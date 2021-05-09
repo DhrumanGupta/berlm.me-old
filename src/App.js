c42b002e357cabc826ef303a97974e5af0b5914b
Berlm <berlm.business@gmail.com>
2021-05-09 17:21:04 +0530

    deploy changes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as HashRouter, Route, Switch} from "react-router-dom";
 import Donate from "./views/Donate";
 import {Redirect} from "react-router";
 import Home from "./views/Home";
@@ -6,13 +6,13 @@ import Home from "./views/Home";
 function App() {
     return (
         <div>
-            <Router>
+            <HashRouter>
                 <Switch>
                     <Route exact path={"/"} component={Home} />
                     <Route path={"/donate"} component={Donate}/>
                     <Route path="*" render={() => <Redirect to={{pathname: "/"}} />} />
                 </Switch>
-            </Router>
+            </HashRouter>
         </div>
     );
 }
