aa51269343031ed1d90dc286c13480ebc469dcd6
Dhruman Gupta <berlm.business@gmail.com>
2021-06-24 02:45:20 +0530

    MAJOR CHANGE. Removing asp.net for database, moving to firebase and netlify

---
+++ src/components/Home.jsx
@@ -0,0 +1,53 @@
+﻿import React, {Suspense} from 'react';
+import Typing from 'react-typist';
+import {MediaData} from "../data/MediaHandles";
+
+const MediaIcon = React.lazy(() => import('./MediaIcon'));
+
+class Home extends React.PureComponent {
+	render() {
+		return (
+			<div className={"d-flex justify-content-center align-items-center flex-column flex-grow-1"}>
+				<div className={"display-1 flex-grow-1 d-flex align-items-center text-center"}>
+					<Typing startDelay={500} cursor={{
+						hideWhenDone: true
+					}}>
+
+						I am Berlm.
+						<Typing.Backspace count={6} delay={750}/>
+						<Typing.Delay ms={500}/>
+
+						a programmer.
+
+						<Typing.Backspace count={13} delay={500}/>
+						<Typing.Delay ms={500}/>
+
+						a designer.
+
+						<Typing.Backspace count={11} delay={500}/>
+						<Typing.Delay ms={500}/>
+
+						a tech enthusiast.
+
+						<Typing.Backspace count={27} delay={700}/>
+
+						Welcome
+					</Typing>
+				</div>
+				<div style={{width: "100px"}} className={"d-flex flex-row align-items-center justify-content-center"}>
+					{
+						MediaData.map((obj) => (
+								<Suspense fallback={" W "} key={obj.class}>
+									<MediaIcon data={obj}/>
+								</Suspense>
+							)
+						)
+					}
+				</div>
+				<hr style={{width: "200px", height: "2px"}} className={"mb-3"}/>
+			</div>
+		);
+	}
+}
+
+export default Home;
---
+++ src/components/MediaIcon.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+class MediaIcon extends React.Component {
+	shouldComponentUpdate() {
+		return false;
+	}
+
+	render() {
+		const data = this.props.data;
+		return (
+			<a href={data.url} target={"_blank"} rel={"noopener noreferrer"} className={`btn btn-outline-${data.color} p-2 mx-1 fa-lg ${data.class}`}/>
+		);
+	}
+}
+
+export default MediaIcon;
