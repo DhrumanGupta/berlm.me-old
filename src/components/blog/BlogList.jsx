6689d8e9833852c66be914f318bd22fa094a822a
Dhruman Gupta <berlm.business@gmail.com>
2021-06-29 16:23:42 +0530

    removed blog test

--- src/components/blog/BlogList.jsx
+++ src/components/blog/BlogList.jsx
@@ -1,86 +1,10 @@
 ﻿import React from 'react';
-import AutoSizer from 'react-virtualized-auto-sizer';
-import {FixedSizeList as List} from 'react-window';
-import InfiniteLoader from "react-window-infinite-loader";
-import BlogCard from "./BlogCard";
-import axios from "axios";
-
-const items = [];
-const requestCache = {};
-let reachedEnd = false;
-
-const isItemLoaded = () => {
-	return false;
-}
-
-const getUrl = index => {
-	return `/api/blog?n=${index}`;
-}
-
-const loadMoreItems = (visibleStartIndex, visibleStopIndex) => {
-	if (reachedEnd) {
-		return;
-	}
-
-	const key = [visibleStartIndex, visibleStopIndex].join(":"); // gives something like 0:5
-	if (requestCache[key]) {
-		return;
-	}
-
-	const length = visibleStopIndex - visibleStartIndex;
-	const visibleRange = [...Array(length).keys()].map(
-		x => x + visibleStartIndex
-	);
-
-	visibleRange.forEach(index => {
-		if (items.length > index) {
-			return;
-		}
-
-		if (reachedEnd) {
-			return;
-		}
-
-		// fetch data from api
-		axios
-			.get(getUrl(index))
-			.then(resp => {
-				console.log(JSON.stringify(resp.data))
-				items.push(resp.data)
-			})
-			.catch(err => {
-				reachedEnd = true;
-			})
-	});
-}
 
 function BlogList() {
 	return (
-		<div className={"container-sm"}>
-			<AutoSizer>
-				{({height, width}) => (
-					<InfiniteLoader
-						isItemLoaded={isItemLoaded}
-						loadMoreItems={loadMoreItems}
-						itemCount={10}
-					>
-						{({onItemsRendered, ref}) => (
-							<List
-								height={height}
-								itemCount={10}
-								itemSize={100}
-								width={width}
-								ref={ref}
-								onItemsRendered={onItemsRendered}
-							>
-								{BlogCard}
-							</List>
-						)
-						}
-					</InfiniteLoader>
-				)
-				}
-			</AutoSizer>
+			<div className="container-sm d-flex justify-content-center align-items-center flex-column flex-grow-1">
+				<p className="text-center display-1">WIP</p>
+				<p className="text-center text-muted">Please try again soon (Almost here!)</p>
 		</div>
 	);
 }
