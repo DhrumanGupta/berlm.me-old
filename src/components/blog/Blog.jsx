8e27ff8b5ef0c6621c9589cd3777622dae3028cb
Dhruman Gupta <berlm.business@gmail.com>
2021-07-02 22:10:15 +0530

    removed deps

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,7 +1,6 @@
 ﻿import React, {useEffect, useState} from 'react';
 import Markdown from "markdown-to-jsx"
 import {useParams} from 'react-router-dom'
-import axios from "axios";
 import Loader from "../Loader";
 import Error404 from "../Error404";
 import Code from "./Code";
